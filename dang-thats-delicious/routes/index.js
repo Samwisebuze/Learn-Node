7f0856687673488156e775107be39fd1776ef113
Samuel Buzas <buzassg@mail.uc.edu>
2019-02-24 14:38:41 -0500

    Video work

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+// Index
+router.get('/', storeController.homePage);
+// Store - Add
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
