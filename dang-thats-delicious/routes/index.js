1b7e9f7f6824bcc9ee4eb3d6b6b0477d0f4d5bef
Samwisebuze <buzassg@mail.uc.edu>
2019-03-06 19:22:27 -0500

    Add error handling to all the routes
    I added the catchErrors HOF to all the endpoints.
    
    It is pruendet to handle errors, even on GET requests. The database could be down, or god forbid a view name changed and wasn't caught in testing.

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -5,12 +5,12 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Index
-router.get('/', storeController.getStores);
-router.get('/stores', storeController.getStores);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 // Store - Get
 router.get('/store/{slug}', catchErrors(storeController.getStore));
 // Store - Add
-router.get('/add', storeController.addStore);
+router.get('/add', catchErrors(storeController.addStore));
 router.post('/add', catchErrors(storeController.createStore));
 
 
