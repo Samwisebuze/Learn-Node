2b01bcfcc2c13b5c6966feddf7b8c11ce1f4bc74
Samwisebuze <buzassg@mail.uc.edu>
2019-03-09 19:46:17 -0500

    Updated moongose connection parser
    mongo is deprecating the old url parser.
    
    The connection method has a workaround to force the system to use the new parser.

--- dang-thats-delicious/start.js
+++ dang-thats-delicious/start.js
@@ -4,20 +4,23 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises -- enables async/await use 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises -- enables async/await use
 mongoose.connection.on('error', (err) => {
+  // eslint-disable-next-line no-console
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
 
-//import all models
+// import all models
 require('./models/Store');
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
+  // eslint-disable-next-line no-console
   console.log(`Express running → PORT ${server.address().port}`);
 });
